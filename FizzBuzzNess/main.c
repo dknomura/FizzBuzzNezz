c0b62d4313c178bb83a85437678e0f9a4c1e39a7
Daniel Nomura <dknomura@gmail.com>
2015-10-06 17:36:08 -0400

    added fizzbuzznezz

--- FizzBuzzNess/main.c
+++ FizzBuzzNess/main.c
@@ -9,7 +9,26 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    printf("Enter a value for Nezz:");
+    int Nezz;
+    scanf("%d", &Nezz);
+    printf("Enter a value 1-10 for Fuzz:");
+    int Fuzz;
+    scanf("%d", &Fuzz);
+    printf("Enter a different value 1-10 for Bizz:");
+    int Bizz;
+    scanf("%d", &Bizz);
+    for (int i = 1; i < Nezz; i++) {
+        if(i % Bizz == 0 && i % Fuzz == 0) {
+            printf("FuzzBizz \n");
+        }
+        else if ( i % Bizz == 0) {
+            printf("Bizz \n");
+        }
+        else if (i % Fuzz == 0) {
+            printf("Fuzz \n");
+        } else {
+            printf("%d \n", i);
+        }
+    }
 }
